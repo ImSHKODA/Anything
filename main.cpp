5681fd28a46484f91a74f9c4da8c390e2239881d
Ilya Shkoda <ilya_shkoda99@mail.ru>
2019-02-25 18:35:03 +0400

    daPlease enter the commit message for your chdnges. Lines starting YES, It's really good he commit.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include<stdio.h>
+#include<iostream>
+
+ using namespace std;
 
 int main()
-{}
+{
+    int x;
+    int y;
+    cout <<"Vvedite chisla" <<endl;
+    cin >> x >> y;
+
+    return 0;
+}
